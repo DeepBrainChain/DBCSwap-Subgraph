d55bed84b0599e8a6c9ada0b112512de92ef57e1
frank0528 <963688200@qq.com>
2024-12-20 09:34:56 +0800

    add DGC token

--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -12,6 +12,7 @@ export const STABLECOIN_IS_TOKEN0 = true
 // usually tokens that many tokens are paired with s
 export const WHITELIST_TOKENS: string[] = [
   WETH_ADDRESS, // WETH
+  '0xC260ed583545d036ed99AA5C76583a99B7E85D26', // DGC
   // '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
   // '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
   // '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
